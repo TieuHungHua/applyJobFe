8b005e7e765b294b8c642e42d0d4ba12ff5f8b28
Hieu Hoang <hoangminhhieu271219@gmail.com>
2024-10-22 14:21:26 +0700

    ok

---
+++ app/Job/page.tsx
@@ -0,0 +1,20 @@
+"use client"
+import SearchBar from '@/components/Search/SearchBar'
+import React from 'react'
+
+const JobPage = () => {
+    return (
+           
+           <div className='bg-custom h-[100vh] '>
+                <div className=" mx-[15%]  pt-[60px]">
+                    <SearchBar></SearchBar>
+                </div>
+                <div >
+
+                </div>
+            </div>
+           
+    )
+}
+
+export default JobPage
---
+++ lib/interface.ts
@@ -0,0 +1,9 @@
+export interface MenuItemType {
+    id: string;
+    title: string;
+    url: string;
+    extention: {
+        urlIcon: string;
+        titleIcon: string;
+    }[]
+}
